e720baf32c07ede2aa777260544acc3b4f4ca877
buzzripper <asmith88@outlook.com>
2025-04-20 21:13:36 -0400

    Troubleshooting

--- src/app1.server/Config/ServiceCollExt.cs
+++ src/app1.server/Config/ServiceCollExt.cs
@@ -49,18 +49,18 @@ public static partial class ServiceCollExt
 
 		var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
 
-		var assyVersion = Assembly.GetExecutingAssembly().GetName().Version;
-		services.AddSwaggerGen(options => {
-			foreach (var description in provider.ApiVersionDescriptions) {
-				options.SwaggerDoc(description.GroupName, new OpenApiInfo {
-					Title = $"{ConfigConst.AppDisplayName} {description.ApiVersion}",
-					Version = description.ApiVersion.ToString(),
-					Description = $"Application server for App1 ({assyVersion})"
-				});
-			}
-		});
+		//var assyVersion = Assembly.GetExecutingAssembly().GetName().Version;
+		//services.AddSwaggerGen(options => {
+		//	foreach (var description in provider.ApiVersionDescriptions) {
+		//		options.SwaggerDoc(description.GroupName, new OpenApiInfo {
+		//			Title = $"{ConfigConst.AppDisplayName} {description.ApiVersion}",
+		//			Version = description.ApiVersion.ToString(),
+		//			Description = $"Application server for App1 ({assyVersion})"
+		//		});
+		//	}
+		//});
 
-		//services.AddSwaggerGen();
+		services.AddSwaggerGen();
 
 		return services;
 	}
