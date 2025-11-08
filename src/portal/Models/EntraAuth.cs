99a66ef02db0dc18eec3927e5ecbaa70bda60723
buzzripper <asmith88@outlook.com>
2025-11-08 17:32:48 -0500

    [deploy]

--- src/portal/Models/EntraAuth.cs
+++ src/portal/Models/EntraAuth.cs
@@ -1,76 +1,77 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Dyvenix.Portal.Models
 {
-    public class TokenIssuanceRequest
-    {
-        public string Type { get; set; }
-        public TokenIssuanceData Data { get; set; }
-    }
+    //public class TokenIssuanceRequest
+    //{
+    //    public string Type { get; set; }
+    //    public TokenIssuanceData Data { get; set; }
+    //}
 
-    public class TokenIssuanceData
-    {
-        [JsonPropertyName("@odata.type")]
-        public string ODataType { get; set; }
+    //public class TokenIssuanceData
+    //{
+    //    [JsonPropertyName("@odata.type")]
+    //    public string ODataType { get; set; }
 
-        public string TenantId { get; set; }
-        public string AuthenticationEventListenerId { get; set; }
-        public string CustomAuthenticationExtensionId { get; set; }
-        public UserInfo User { get; set; }
-        public AuthenticationContext AuthenticationContext { get; set; }
-    }
+    //    public string TenantId { get; set; }
+    //    public string AuthenticationEventListenerId { get; set; }
+    //    public string CustomAuthenticationExtensionId { get; set; }
+    //    public UserInfo User { get; set; }
+    //    public AuthenticationContext AuthenticationContext { get; set; }
+    //}
 
-    public class UserInfo
-    {
-        public string Id { get; set; }
-        public string UserPrincipalName { get; set; }
-        public string Email { get; set; }
-        public string DisplayName { get; set; }
-        public string GivenName { get; set; }
-        public string Surname { get; set; }
-    }
+    //public class UserInfo
+    //{
+    //    public string Id { get; set; }
+    //    public string UserPrincipalName { get; set; }
+    //    public string Email { get; set; }
+    //    public string DisplayName { get; set; }
+    //    public string GivenName { get; set; }
+    //    public string Surname { get; set; }
+    //}
 
-    public class AuthenticationContext
-    {
-        public string CorrelationId { get; set; }
-        public ClientInfo Client { get; set; }
-        public string Protocol { get; set; }
-        public ServicePrincipalInfo ClientServicePrincipal { get; set; }
-    }
+    //public class AuthenticationContext
+    //{
+    //    public string CorrelationId { get; set; }
+    //    public ClientInfo Client { get; set; }
+    //    public string Protocol { get; set; }
+    //    public ServicePrincipalInfo ClientServicePrincipal { get; set; }
+    //}
 
-    public class ClientInfo
-    {
-        public string Ip { get; set; }
-        public string Locale { get; set; }
-        public string Market { get; set; }
-    }
+    //public class ClientInfo
+    //{
+    //    public string Ip { get; set; }
+    //    public string Locale { get; set; }
+    //    public string Market { get; set; }
+    //}
 
-    public class ServicePrincipalInfo
-    {
-        public string Id { get; set; }
-        public string AppId { get; set; }
-        public string DisplayName { get; set; }
-    }
+    //public class ServicePrincipalInfo
+    //{
+    //    public string Id { get; set; }
+    //    public string AppId { get; set; }
+    //    public string DisplayName { get; set; }
+    //}
 
-    // Response model
-    public class TokenIssuanceResponse
-    {
-        public ResponseData Data { get; set; }
-    }
+    //// Response model
+    //public class TokenIssuanceResponse
+    //{
+    //    public ResponseData Data { get; set; }
+    //}
 
-    public class ResponseData
-    {
-        public ClaimsAction[] Actions { get; set; }
-    }
+    //public class ResponseData
+    //{
+    //    public ClaimsAction[] Actions { get; set; }
+    //}
 
-    public class ClaimsAction
-    {
-        [JsonPropertyName("@odata.type")]
-        public string ODataType => "microsoft.graph.tokenIssuanceStart.provideClaimsForToken";
+    //public class ClaimsAction
+    //{
+    //    [JsonPropertyName("@odata.type")]
+    //    public string ODataType => "microsoft.graph.tokenIssuanceStart.provideClaimsForToken";
 
-        public Dictionary<string, object> Claims { get; set; }
-    }
+    //    public Dictionary<string, object> Claims { get; set; }
+    //}
 
     //---------------------------------------------
 
@@ -99,20 +100,50 @@ namespace Dyvenix.Portal.Models
         public Dictionary<string, object> Claims { get; set; } = new();
     }
 
-    // --- Minimal request shape (adjust if you need fields) ---
-    public sealed class TokenIssuanceStartRequest
+
+    public class TokenIssuanceStartRequest
     {
         [JsonPropertyName("data")]
-        public TokenIssuanceStartRequestData? Data { get; set; }
+        public TokenIssuanceStartRequestData Data { get; set; } = new TokenIssuanceStartRequestData();
     }
-    public sealed class TokenIssuanceStartRequestData
+
+    public class TokenIssuanceStartRequestData
     {
-        [JsonPropertyName("tenantId")] public string? TenantId { get; set; }
-        [JsonPropertyName("user")] public object? User { get; set; }
-        [JsonPropertyName("context")] public object? Context { get; set; }
+        [JsonPropertyName("@odata.type")]
+        public string ODataType { get; set; } = default!;
+
+        [JsonPropertyName("authenticationContext")]
+        public AuthenticationContext? AuthenticationContext { get; set; }
+
+        [JsonPropertyName("user")]
+        public JsonElement? User { get; set; }
+
+        [JsonPropertyName("application")]
+        public JsonElement? Application { get; set; }
+
+        [JsonPropertyName("session")]
+        public JsonElement? Session { get; set; }
+
+        [JsonPropertyName("tenantId")]
+        public string? TenantId { get; set; }
+
+        [JsonPropertyName("context")]
+        public JsonElement? Context { get; set; }
+
+        [JsonPropertyName("additionalData")]
+        public JsonElement? AdditionalData { get; set; }
     }
 
-
+    public class AuthenticationContext
+    {
+        [JsonPropertyName("correlationId")]
+        public string? CorrelationId { get; set; }
+        [JsonPropertyName("authenticationEventId")]
+        public string? AuthenticationEventId { get; set; }
+        [JsonPropertyName("redirectUri")]
+        public string? RedirectUri { get; set; }
+        // … any other fields
+    }
 
 
 }
