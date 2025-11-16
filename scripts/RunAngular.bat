2b53032d0aa0cdad40456f072bd7e1a567aa34d6
buzzripper <asmith88@outlook.com>
2025-11-16 12:03:17 -0500

    Refreshed Angular code to latest Demo version

--- scripts/RunAngular.bat
+++ scripts/RunAngular.bat
@@ -1,10 +1,44 @@
+@echo off
+setlocal EnableDelayedExpansion
 
-set "SCRIPT_DIR=%~dp0"
+:: Get the first argument
+set arg=%1
 
-CD "%SCRIPT_DIR%..\src\ui\angular"
- 
+:: Handle empty argument
+:: if "%arg%"=="" (
+::     echo No argument provided. Please specify one of: portal, starter, demo.
+::     exit /b 1
+:: )
 
-ng serve
+echo -------  %arg%  ------------
 
-::PRINT %CD_DIR%
-::PAUSE
+:: Determine the directory
+if /i "%arg%"=="app1" (
+    cd /d "%~dp0..\src\ui\Angular"
+    set port=4200
+
+) else if /i "%arg%"=="starter" (
+    cd /d "D:\Code\Reference\Fuse\starter"
+    set port=4200
+
+) else if /i "%arg%"=="demo" (
+    cd /d "D:\Code\Reference\Fuse\demo"
+    set port=4200
+
+) else (
+    echo Invalid argument: %arg%
+    echo Valid options are: portal, starter, demo.
+    PAUSE
+    exit /b 1
+)
+
+call ng serve --port %port%
+
+IF ERRORLEVEL 1 (
+    echo Angular server failed to start.
+    PAUSE
+    exit /b 1
+)
+
+::--ssl-key C:\ProgramData\Certs\localhost.key --ssl-cert C:\ProgramData\Certs\localhost.crt
+
--- src/ui/Angular/src/main.ts
+++ src/ui/Angular/src/main.ts
@@ -1,14 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from 'app/app.component';
 import { appConfig } from 'app/app.config';
-import { provideOAuthClient } from 'angular-oauth2-oidc';
 
-bootstrapApplication(AppComponent, {
-    ...appConfig,
-    providers: [
-        ...(appConfig.providers || []),
-        provideOAuthClient()
-    ]
-}).catch((err) =>
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err)
 );
